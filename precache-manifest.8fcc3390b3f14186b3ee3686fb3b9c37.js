b60bfed2f1f37bc278bc083f4da76af66a33ee09
santiagoidu <sr_santiago@icloud.com>
2021-09-12 21:16:46 -0300

    Updates

---
+++ precache-manifest.8fcc3390b3f14186b3ee3686fb3b9c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a8d962eb517e57e090425522a4cb1c",
+    "url": "/React-Marvel/index.html"
+  },
+  {
+    "revision": "387bc4b89335f1791155",
+    "url": "/React-Marvel/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "387bc4b89335f1791155",
+    "url": "/React-Marvel/static/js/2.43cb7332.chunk.js"
+  },
+  {
+    "revision": "279d106f8be8bd540dd4",
+    "url": "/React-Marvel/static/js/main.d2f90624.chunk.js"
+  },
+  {
+    "revision": "b42a851fac28010a97f5",
+    "url": "/React-Marvel/static/js/runtime-main.6381b86a.js"
+  },
+  {
+    "revision": "cc3a8cb9ea19776093219ce81cc43c49",
+    "url": "/React-Marvel/static/media/marvel.cc3a8cb9.jpg"
+  }
+]);
